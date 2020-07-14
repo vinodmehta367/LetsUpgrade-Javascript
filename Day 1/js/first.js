6195e2260f34262f2ada75e04d59677ba5ebe02d
vinodmehta367 <51881821+vinodmehta367@users.noreply.github.com>
2020-07-14 22:46:28 +0530

    Add files via upload

---
+++ Day 1/js/first.js
@@ -0,0 +1,34 @@
+var a=["jay",43,"cs"]
+var b=["vijay",44,"civil"]
+
+console.table({a,b});
+
+
+//var example
+var name="sanjay";
+name="jay";
+console.log(name);
+
+//let example
+var name="sanjay";
+{
+    let name = "sanjana";
+    console.log("the value of name in local scope:",name)
+}
+console.log("the global value of name  ",name);
+
+//const example
+// const person;
+// person="Ajay";
+// console.log(person);
+
+// const newPerson= "Ajay"
+// newPerson ="vijay"
+// console.log(newPerson);
+
+//Array example
+const names=["amar","akbar"];
+names.push("anthony");
+console.log(names);
+
+
